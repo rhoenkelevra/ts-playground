124e850762f1a0faeb6ef48bf8d110c579809988
Rene Rhoen <renerhoen@hotmail.com>
2021-06-12 11:06:51 +0900

    ts arrow function example

--- src/function.ts
+++ src/function.ts
@@ -1,5 +1,11 @@
+// Using function declaration
 function addNumbers(num1: number, num2: number): number {
     return num1 + num2;
 }
+
+// Using an arrow function
+export const joinStrings = (str1: string, str2: string): string => {
+    return `${str1} ${str2}`;
+};
 
 export default addNumbers;
--- src/functions-test.ts
+++ src/functions-test.ts
@@ -1,4 +1,5 @@
-import addNumbers from './function';
+import addNumbers, { joinStrings } from './function';
 
 const sumNumbers = addNumbers;
 console.log(sumNumbers(1, 2));
+console.log(joinStrings('hello', 'world'));
